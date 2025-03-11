f731bae94ce175aca2aac7883fe06f0635bc9a22
VictorHOgawa <victor.ogawa@gmail.com>
2025-03-11 16:34:54 -0300

    commit

--- src/app/(dashboard)/(home)/components/FormSheet.tsx
+++ src/app/(dashboard)/(home)/components/FormSheet.tsx
@@ -9,6 +9,7 @@ import { Step0 } from "./Step0";
 import { Step1 } from "./Step1";
 import { Step2 } from "./Step2";
 import { Step3 } from "./Step3";
+import { Step4 } from "./Step4";
 
 interface FormSheetProps {
   open: boolean;
@@ -96,8 +97,10 @@ export function FormSheet({ open, setOpen }: FormSheetProps) {
           <Step1 />
         ) : currentStep === 2 ? (
           <Step2 />
-        ) : (
+        ) : currentStep === 3 ? (
           <Step3 />
+        ) : (
+          <Step4 />
         )}
         <button
           onClick={HandleNextStep}
