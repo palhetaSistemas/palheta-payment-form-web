6c47dc1a67a2019394596644822912ce8b5b5188
maycon600 <mayconvasconcellos2010@hotmail.com>
2025-04-17 13:34:05 -0400

    fix(style): add h-screen to avoid overflow scren

--- src/app/(dashboard)/(home)/page.tsx
+++ src/app/(dashboard)/(home)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useEffect } from "react";
 import Lenis from "lenis";
+import { useEffect } from "react";
 import { StartView } from "./components/StartView";
 
 export default function Dashboard() {
@@ -14,7 +14,7 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div className="bg-[#123262] lg:bg-[#07234E] p-2 relative flex w-full lg:w-[500px] lg:mx-auto items-center flex-col min-h-screen">
+    <div className="bg-[#123262] lg:bg-[#07234E] p-2 relative flex w-full lg:w-[500px] lg:mx-auto items-center flex-col min-h-screen max-h-screen">
       <StartView />
     </div>
   );
